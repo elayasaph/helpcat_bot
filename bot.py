7e1c3a3555c5a2572ba2870168512d11c104227b
Elaya Saph <elaya777@mail.ru>
2026-07-31 22:49:36 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -59,9 +59,10 @@ CATS = {
 
 PAYMENT_INFO = (
     "💳 <b>Реквизиты для помощи котикам:</b>\n\n"
-    "🟢 <b>Kaspi Gold/Halyk Bank:\n"
+    "🔴 <b>Kaspi Gold/Halyk Bank:\n"
     "</b><code> +77074040039</code> (Әлия С.)\n"
-    "🟡 <b>Карта:</b><code> 4405 6397 7249 6939</code>\n"
+    "🟢 <b>Карта:</b><code> 4405 6397 7249 6939</code>\n"
+    "🌎 </b>PayPal:</b><code> helpcatkz@gmail.com </code>\n""
     "<i> В комментарии перевода укажите имя котика.\n"
     Спасибо за поддержку!</i>"
 )
